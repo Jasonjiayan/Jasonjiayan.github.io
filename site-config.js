ca87ed2f1df31982e65574eea25a25d29ec50311
zhichenglu <jiayanjason@gmail.com>
2025-10-03 16:55:43 +0800

    update visitor map

--- site-config.js
+++
@@ -1,7 +0,0 @@
-window.siteConfig = window.siteConfig || {};
-
-// Update this URL after registering your own visitor globe.
-// Example (ClustrMaps): "//clustrmaps.com/globe.js?d=YOUR_GENERATED_KEY"
-// window.siteConfig.visitorGlobeSrc = '//clustrmaps.com/globe.js?d=REPLACE_WITH_YOUR_KEY';
-window.siteConfig.visitorGlobeSrc = '//clustrmaps.com/map_v2.js?d=l2F3XiAlUEcAP9nlu0KuD5LW1HiF0JB94i6BBkMJQO4&cl=ffffff&w=a';
-
